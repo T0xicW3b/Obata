144ff58a796d02c0df825a181e013a9f5c69d79f
Rmaain <intornanow@gmail.com>
2025-11-18 19:12:46 -0300

    set

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ from game import Question, gen_questions
 
 df = pd.read_csv('data.csv')
 
-@app.get("/")
+@app.route("/")
 def index():
     allPlants = len(df)
     return render_template("index.html")
@@ -21,31 +21,30 @@ def search_plant():
     
     else:
         return "Planta não encontrada! <a href='/'>Voltar</a>"
-    
-@app.get("/game")
-def game():
-    questions = gen_questions("data.csv", 12, "Nome Popular", "Nome Científico")
-    session["questions"] = [
-        {
-            "id": q.id,
-            "ask": q.ask,
-            "a": q.a,
-            "b": q.b,
-            "c": q.c,
-            "d": q.d,
-            "e": q.e,
-            "correct": q.correct,
-        }
-        for q in questions
-    ]
-    return render_template("game.html", questions=questions)
 
-@app.route("/result", methods=["POST"])
-def result():
-    questions = session.get("questions", [])
-    score = 0
-    for i in questions:
-        r = request.form.get(str(i["id"]))
-        if r == i["correct"]:
-            score += 1
-    return render_template("result.html", score=score)
+   
+@app.route("/game", methods=["POST", "GET"])
+def game():
+    if "index" not in session:
+        questions = gen_questions("data.csv", 12, "Nome Popular", "Nome Científico")
+        session["questions"] = [
+            {"ask": q.ask, "a": q.a, "b":q.b, "c": q.c, "d": q.d, "e": q.e, "correct": q.correct}
+            for q in questions
+        ]
+        session["index"] = 0
+        session["score"] = 0
+
+    if request.method == "POST":
+        r = request.form.get("qu")
+        correct = session['questions'][session["index"]]['correct']
+        if r and r.strip().lower() == correct.strip().lower():
+            session["score"] += 1
+        session["index"] += 1
+
+    if session["index"] >= len(session['questions']):
+        score = session["score"]
+        session.clear()
+        return f"Você acertou {score} questões!"
+
+    q = session['questions'][session["index"]]
+    return render_template("game.html", question=q)
